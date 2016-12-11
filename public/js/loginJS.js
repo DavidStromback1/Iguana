d31e712b04c82f3cba0e558fdb1ef8668aed59a8
Bernie <franziska.bernie@gmail.com>
2016-12-11 23:07:29 +0100

    Small :fire: :fire_engine:
    No more annoyances when trying to register & forgot password

--- public/js/loginJS.js
+++ public/js/loginJS.js
@@ -1,38 +1,59 @@
 /* 
-    Created on : 03-Dec-2016, 21:55:38
-    Author     : Bernie
-*/
+ Created on : 03-Dec-2016, 21:55:38
+ Author     : Bernie
+ */
+var $helpBlock = $(".help-block");
+var $hasError = $(".has-error");
+var visited = localStorage.visited;
+var $modalLog = $(".mainOverLayLog");
+var $modalReg = $(".mainOverLayReg");
+var $LogBtn = $(".loginBtn");
+var $RegBtn = $(".registerBtn");
+var $OverLay = $(".overLay");
+var $FormClearName = $("#name");
+var $FormClearEmail = $("#email");
+var $FormClearEmailREG = $modalReg.find("#email");
 
-    var $modalLog = $(".mainOverLayLog");
-    var $modalReg = $(".mainOverLayReg");
-    var $LogBtn = $(".loginBtn");
-    var $RegBtn = $(".registerBtn");
-    var $OverLay = $(".overLay");
-    
 
-function btnClickedLog(){
-        $modalLog.addClass("active-mainOverLay");
-    }
-function btnClickedReg(){
-        $modalReg.addClass("active-mainOverLay");
-    }
-    
-function overLayDisappearLog(){
-        console.log("Hello LOG");
-        $modalLog.removeClass("active-mainOverLay");
+function btnClickedLog() {
+    $FormClearEmail.val("");
+    localStorage.setItem("visited", "login");
+    $modalLog.addClass("active-mainOverLay");
+
+}
+function btnClickedReg() {
+    $FormClearName.val("");
+    $FormClearEmailREG.val("");
+    localStorage.setItem("visited", "register");
+    $modalReg.addClass("active-mainOverLay");
 }
 
-function overLayDisappearReg(){
-        console.log("Hello REG");
-        $modalReg.removeClass("active-mainOverLay");
+function overLayDisappearLog() {
+    $modalLog.removeClass("active-mainOverLay");
+    $helpBlock.remove();
+    $hasError.removeClass("has-error");
 }
-    
- $(document).ready(function(){
-    
-     $LogBtn.on("click", btnClickedLog);
-     $RegBtn.on("click", btnClickedReg);
-     $OverLay.on("click", overLayDisappearLog);
-     $OverLay.on("click", overLayDisappearReg);
- });
- 
+
+function overLayDisappearReg() {
+    $modalReg.removeClass("active-mainOverLay");
+    $helpBlock.remove();
+    $hasError.removeClass("has-error");
+}
+
+$(document).ready(function () {
+    checkErrorState();
+    $LogBtn.on("click", btnClickedLog);
+    $RegBtn.on("click", btnClickedReg);
+    $OverLay.on("click", overLayDisappearLog);
+    $OverLay.on("click", overLayDisappearReg);
+});
+
+function checkErrorState() {
+    if ($(".help-block").length >= 1 && visited === "login") {
+        btnClickedLog();
+    } else if ($(".help-block").length >= 1 && visited === "register") {
+        btnClickedReg();
+    }
+}
+
  
